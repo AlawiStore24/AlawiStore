2a67e6e2911a93be261b1fbc47d0eb01d5a10348
AlawiStore24 <159411067+AlawiStore24@users.noreply.github.com>
2024-02-09 04:18:51 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -83,7 +83,7 @@ gem install lolcat
 apt install wondershaper -y
 clear
 # REPO    
-    REPO="https://raw.githubusercontent.com/AlawiStore24/AlawiStore/"
+    REPO="https://raw.githubusercontent.com/AlawiStore24/AlawiStore/main/"
 
 ####
 start=$(date +%s)
@@ -266,7 +266,7 @@ restart_system(){
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m" 
 clear
-izinsc="https://raw.githubusercontent.com/AlawiStore24/AlawiStore/REGISTRASI"
+izinsc="https://raw.githubusercontent.com/AlawiStore24/AlawiStore/main/REGISTRASI"
 # USERNAME
 rm -f /usr/bin/user
 username=$(curl $izinsc | grep $MYIP | awk '{print $2}')
@@ -526,7 +526,7 @@ print_success "Password SSH"
 function udp_mini(){
 clear
 print_install "Memasang Service Limit IP & Quota"
-wget -q https://raw.githubusercontent.com/AlawiStore24/AlawiStore/config/fv-tunnel && chmod +x fv-tunnel && ./fv-tunnel
+wget -q https://raw.githubusercontent.com/AlawiStore24/AlawiStore/main/config/fv-tunnel && chmod +x fv-tunnel && ./fv-tunnel
 
 # // Installing UDP Mini
 mkdir -p /usr/local/kyt/
